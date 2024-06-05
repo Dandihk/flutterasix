77d7d544ec874242b0f7c7b108726b4c4333a6c4
n00b38d <n00b38d@gmail.com>
2024-06-05 14:01:48 +0000

    asix 2

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter yanto Home Page'),
     );
   }
 }
@@ -122,4 +122,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  
 }
